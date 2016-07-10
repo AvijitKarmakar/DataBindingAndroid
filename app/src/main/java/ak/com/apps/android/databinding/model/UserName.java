8daff1d5b10faa1acd35751d50776b374aa432ea
Avijit Karmakar <karmakar.avijit93@gmail.com>
2016-07-10 16:41:29 +0530

    UserName data model created

---
+++ app/src/main/java/ak/com/apps/android/databinding/model/UserName.java
@@ -0,0 +1,36 @@
+package ak.com.apps.android.databinding.model;
+
+/**
+ * Created by USER on 10-07-2016.
+ */
+public class UserName {
+
+    private String firstName;
+    private String middleName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
